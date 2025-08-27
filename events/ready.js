f51783a9b48a446e72ef4b2bff5d3118d7965b72
luwuxy <marjosephbijer123@gmail.com>
2025-08-27 23:00:32 +0800

    test

--- events/ready.js
+++ events/ready.js
@@ -5,6 +5,6 @@ module.exports = {
     name: Events.ClientReady,
     once: true,
     execute(client) {
-        console.log(`Ready! Logged in as ${client.user.tag}`);
+        console.log(`Ready! Logged in as ${client.user.tag}.`);
     },
 };
