d3c0080f25e3f0c54871358cc899fa445f210d52
luwuxy <marjosephbijer123@gmail.com>
2025-08-28 22:10:10 +0800

    fixed env variables not loading

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,6 +1,7 @@
 const { REST, Routes } = require('discord.js');
 const fs = require('node:fs');
 const path = require('node:path');
+require('dotenv').config();
 
 const commands = [];
 // Grab all the command folders from the commands directory you created earlier
