2fa30bdbbf25ffec8fbdcb282becd2b56bef0bd2
luwuxy <marjosephbijer123@gmail.com>
2025-08-28 11:50:26 +0800

    idk

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,16 +6,16 @@ module.exports = {
     async execute(member) {
         const welcomeEmbed = new EmbedBuilder()
             .setColor('#ffefd6')
-            .setTitle('Welcome to Pâtisserie Plain!')
+            .setTitle(`Welcome to ${member.guild.name}!`)
             .setAuthor({
                 name: member.user.tag,
                 iconURL: member.displayAvatarURL({ format: 'png', size: 128})
             })
-            .setThumbnail('https://cdn.discordapp.com/icons/1373475501749960776/018439d314abdad0fc0462bb058b6b95.png?size=128')
-            .setDescription('Before you start talking, be sure to check out the <#1405387046649991251> and introduce yourself in <#1405387163687977071>!')
+            .setThumbnail(member.guild.iconURL({size: 128, extension: 'png'}))
+            .setDescription(`Before you start talking, be sure to check out the <#${process.env.CHANNEL_ID2}> and introduce yourself in <#${process.env.CHANNEL_ID3}>!`)
             .setImage('https://c.tenor.com/ovJxRSyjPQEAAAAC/tenor.gif');
 
-        await member.guild.channels.fetch('1405931638415818783')
+        await member.guild.channels.fetch(process.env.CHANNEL_ID)
             .then(channel => {
                 channel.send({
                     content: `<@${member.id}>`,
