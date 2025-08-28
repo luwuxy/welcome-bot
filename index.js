760e114deccce5eeea0d369d288a1190f854d10d
luwuxy <marjosephbijer123@gmail.com>
2025-08-28 22:13:35 +0800

    added DirectMessages intent

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const client = new Client({
     intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
+		GatewayIntentBits.DirectMessages,
         GatewayIntentBits.MessageContent,
         GatewayIntentBits.GuildMembers,
     ]
