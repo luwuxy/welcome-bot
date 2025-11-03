f8ab66c4dd17b69913d4c9c7d6d29ee7a75293a5
luwuxy <marjosephbijer123@gmail.com>
2025-11-03 23:53:49 +0800

    GRRRRR

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,21 +5,21 @@ module.exports = {
     name: Events.GuildMemberUpdate,
     async execute(oldMember, newMember) {
         if (oldMember.pending === true && newMember.pending === false) {
-            const channel = member.client.modmailWelcome;
+            const channel = newMember.client.modmailWelcome;
 
             const welcomeEmbed = new EmbedBuilder()
                 .setColor('#ffefd6')
-                .setTitle(`Welcome to ${member.guild.name}!`)
+                .setTitle(`Welcome to ${newMember.guild.name}!`)
                 .setAuthor({
-                    name: member.user.tag,
-                    iconURL: member.displayAvatarURL({ format: 'png', size: 128 })
+                    name: newMember.user.tag,
+                    iconURL: newMember.displayAvatarURL({ format: 'png', size: 128 })
                 })
-                .setThumbnail(member.guild.iconURL({ size: 128, extension: 'png' }))
+                .setThumbnail(newMember.guild.iconURL({ size: 128, extension: 'png' }))
                 .setDescription(`Before you start talking, be sure to check out the <#${process.env.CHANNEL_ID2}> and introduce yourself in <#${process.env.CHANNEL_ID3}>!`)
                 .setImage('https://c.tenor.com/ovJxRSyjPQEAAAAC/tenor.gif');
 
             channel.send({
-                content: `<@${member.id}>`,
+                content: `<@${newMember.id}>`,
                 embeds: [welcomeEmbed]
             });
         }
