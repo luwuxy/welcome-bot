847d1c39fc0a7cd69f717491e766d38216c0016e
luwuxy <marjosephbijer123@gmail.com>
2025-11-03 23:45:44 +0800

    fix

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -3,25 +3,25 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
     name: Events.GuildMemberUpdate,
-    async execute(member) {
-        if (member.pending === false) return;
-        
-        const channel = member.client.modmailWelcome;
+    async execute(oldMember, newMember) {
+        if (oldMember.pending && !newMember.pending) {
+            const channel = member.client.modmailWelcome;
 
-        const welcomeEmbed = new EmbedBuilder()
-            .setColor('#ffefd6')
-            .setTitle(`Welcome to ${member.guild.name}!`)
-            .setAuthor({
-                name: member.user.tag,
-                iconURL: member.displayAvatarURL({ format: 'png', size: 128})
-            })
-            .setThumbnail(member.guild.iconURL({size: 128, extension: 'png'}))
-            .setDescription(`Before you start talking, be sure to check out the <#${process.env.CHANNEL_ID2}> and introduce yourself in <#${process.env.CHANNEL_ID3}>!`)
-            .setImage('https://c.tenor.com/ovJxRSyjPQEAAAAC/tenor.gif');
+            const welcomeEmbed = new EmbedBuilder()
+                .setColor('#ffefd6')
+                .setTitle(`Welcome to ${member.guild.name}!`)
+                .setAuthor({
+                    name: member.user.tag,
+                    iconURL: member.displayAvatarURL({ format: 'png', size: 128 })
+                })
+                .setThumbnail(member.guild.iconURL({ size: 128, extension: 'png' }))
+                .setDescription(`Before you start talking, be sure to check out the <#${process.env.CHANNEL_ID2}> and introduce yourself in <#${process.env.CHANNEL_ID3}>!`)
+                .setImage('https://c.tenor.com/ovJxRSyjPQEAAAAC/tenor.gif');
 
-        channel.send({
-            content: `<@${member.id}>`,
-            embeds: [welcomeEmbed]
-        });
+            channel.send({
+                content: `<@${member.id}>`,
+                embeds: [welcomeEmbed]
+            });
+        }
     },
 };
